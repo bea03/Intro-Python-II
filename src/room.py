93b4fba4685164f5491e613fdd9f26b06e65dbb9
Nicole Beamer <tulip@Nicoles-iMac.hsd1.ut.comcast.net>
2020-06-03 14:02:53 -0600

    rooms

--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description)
+        #needs a name
+        self.name = name
+        #needs a description
+        self.description = description
+        #needs connected rooms, default is set to none.
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
